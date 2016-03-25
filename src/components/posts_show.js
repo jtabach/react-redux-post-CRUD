351d63cb9eac580147a4e99239db42476acec419
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-24 17:25:17 -0700

    fetches post by params

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
   render() {
     return (
       <div>Show post {this.props.params.id}</div>
@@ -8,4 +14,4 @@ class PostsShow extends Component {
   }
 }
 
-export default PostsShow;
+export default connect(null, { fetchPost })(PostsShow);
